2f7e45cbf6f1ed2472c72b80efd824c893fbae1a
FranInfanti <francisco.infanti@gmail>
2024-05-17 20:32:10 -0300

    Second Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sistemas-l
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/sistemas_l/core_test.clj
@@ -0,0 +1,7 @@
+(ns sistemas-l.core-test
+  (:require [clojure.test :refer :all]
+            [sistemas-l.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
