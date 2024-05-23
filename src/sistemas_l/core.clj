5d09c220ea76c67eada88128c1fcb3abc51ab230
FranInfanti <francisco.infanti@gmail>
2024-05-23 18:34:44 -0300

    Primeros Pasos

--- src/sistemas_l/core.clj
+++ src/sistemas_l/core.clj
@@ -1,6 +1,20 @@
-(ns sistemas-l.core)
+(ns sistemas-l.core
+  (:gen-class)
+  (:require [clojure.java.io :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn read-file [file]
+  "Recibe un archivo por parametro, si existe, lee su contenido y escribe como un vector"
+  (try
+    (with-open [reader (io/reader file)] (apply conj [] (line-seq reader)))
+    (catch Exception _)))
+
+(defn gen-patron [axioma remplazos it]
+  "Se recibe el axioma y como se va remplazado"
+  )
+
+(defn -main [inputFile it outputFile]
+  (let [
+        info (read-file inputFile)
+        patron (gen-patron (first (rest info)) (vec (rest (rest info))) it)
+        ]
+    (print info)))
