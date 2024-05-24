7670111bef6d6953d1b1949e4bf66e31387ce026
FranInfanti <francisco.infanti@gmail>
2024-05-23 21:30:46 -0300

    Termino de implementar lo del hash

--- src/sistemas_l/core.clj
+++ src/sistemas_l/core.clj
@@ -8,13 +8,25 @@
     (with-open [reader (io/reader file)] (apply conj [] (line-seq reader)))
     (catch Exception _)))
 
+(defn hash-create [remp]
+  (println remp)
+  (if (empty? remp) {}
+                   (let [key (first (seq (apply str (first remp))))
+                         value (apply str (nnext (seq (apply str (first remp)))))
+                         ]
+
+                     (merge (hash-map key value) (hash-create (vec (rest remp)))))
+    )
+  )
+
+
 (defn gen-patron [axioma remplazos it]
-  "Se recibe el axioma y como se va remplazado"
+  (if (zero? it) (apply str axioma)
+                 ())
   )
 
 (defn -main [inputFile it outputFile]
-  (let [
-        info (read-file inputFile)
-        patron (gen-patron (first (rest info)) (vec (rest (rest info))) it)
+  (let [info (read-file inputFile)
+        patron (gen-patron (seq (first (rest info))) () it)
         ]
-    (print info)))
+    ))
