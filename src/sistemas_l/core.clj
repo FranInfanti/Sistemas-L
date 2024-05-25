6da39067beaf15ca80fa5d5f3a43c957bc5f4990
FranInfanti <francisco.infanti@gmail>
2024-05-25 10:54:22 -0300

    Uno lo que hicimos, y pruebo algunas cosas

--- src/sistemas_l/core.clj
+++ src/sistemas_l/core.clj
@@ -8,37 +8,33 @@
     (with-open [reader (io/reader file)] (apply conj [] (line-seq reader)))
     (catch Exception _)))
 
-(defn gen-patron [axioma reemp it]
-  (if (zero? it)
-    axioma
-    (gen-patron (apply str (sequence (replace reemp (vec axioma)))) reemp (dec it))
-    )
+(defn gen-patron [axioma remp it]
+  (if (zero? it) axioma
+                 (gen-patron (apply str (sequence (replace remp (vec axioma)))) remp (dec it))
+                 )
   )
 
 (defn hash-create [remp]
-  (println remp)
+  "Recibe un vector con los remplazos (remp) y lo separa en un hash, con key el caracter asociado al remplazo
+  y value el remplazo en si"
   (if (empty? remp) {}
-                   (let [key (first (seq (apply str (first remp))))
-                         value (apply str (nnext (seq (apply str (first remp)))))
-                         ]
-
+                    (let [key (first (seq (apply str (first remp))))
+                          value (apply str (nnext (seq (apply str (first remp)))))
+                          ]
                      (merge (hash-map key value) (hash-create (vec (rest remp)))))
-    )
+                    )
   )
 
-(defn gen-texto-svg [angulo patron]
-  (println angulo)
-  patron
-  )
+(defn gen-texto-svg [angulo patron])
 
 (defn write-file [outputFile texto]
-  (println texto)
   )
 
 (defn -main [inputFile it outputFile]
+  "Recibe el archio .sl, la cantidad de iteraciones a realizar y el archivo .xml donde se escribe el resultado"
   (let [info (read-file inputFile)
         angulo (Double/parseDouble (first info))
-        patron (gen-patron (first (rest info)) (hash-create (vec (drop 2 info))) it)
+        patron (gen-patron (first (rest info)) (hash-create (vec (nnext info))) it)
         ]
     (write-file outputFile (gen-texto-svg angulo patron))
     ))
