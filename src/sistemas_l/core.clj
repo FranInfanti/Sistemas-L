956b606a3d6ac61c480529fb2767a23fa438f79f
alanifranco <alanifranco2003@gmail.com>
2024-05-29 18:06:50 -0300

    Idea del loop con la pila de tortuga del gen-texto-svg

--- src/sistemas_l/core.clj
+++ src/sistemas_l/core.clj
@@ -26,10 +26,30 @@
     )
   )
 
-(defn gen-texto-svg [angulo patron]
-  (println angulo)
-  patron
+(defn tortuga [x y angulo]
+  {:x x,  :y y ,:angulo angulo})
+
+(defn gen-texto-svg [patron pila-tortuga]
+  (println pila-tortuga)
+  (if (empty? patron)
+    " "
+    (let [letra (first patron)
+          resto (rest patron)
+          tortuga (first pila-tortuga)
+          resto-pila (rest pila-tortuga)]
+      (cond
+        (= letra \F) (str "Avanzo. "           (gen-texto-svg resto (cons tortuga resto-pila))) ;una nueva lista con la tortuga modificada en pos en vez de la tortuga
+        (= letra \f) (str "Avanzo dibujando. " (gen-texto-svg resto (cons tortuga resto-pila))) ;lo mismo
+        (= letra \+) (str "Giro Der. "         (gen-texto-svg resto (cons tortuga resto-pila))) ;una nueva lista con la tortuga modificada en angulo en vez de la tortuga
+        (= letra \-) (str "Giro Izq. "         (gen-texto-svg resto (cons tortuga resto-pila))) ;lo mismo
+        (= letra \|) (str "Giro 180. "         (gen-texto-svg resto (cons tortuga resto-pila))) ;lo mismo
+        (= letra \[) (str "Apilo Tortuga. "    (gen-texto-svg resto (cons tortuga pila-tortuga)))
+        (and (not (empty? resto-pila)) (= letra  \])) (str "Desapilo Tortuga. " (gen-texto-svg resto resto-pila))
+        :else (gen-texto-svg resto pila-tortuga))
+        )
+      )
   )
+
 
 (defn write-file [outputFile texto]
   (println texto)
@@ -39,6 +59,7 @@
   (let [info (read-file inputFile)
         angulo (Double/parseDouble (first info))
         patron (gen-patron (first (rest info)) (hash-create (vec (drop 2 info))) it)
+        pila-tortuga (list (tortuga 0 0 angulo))
         ]
-    (write-file outputFile (gen-texto-svg angulo patron))
+    (write-file outputFile (gen-texto-svg patron pila-tortuga))
     ))
